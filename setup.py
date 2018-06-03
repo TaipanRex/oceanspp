97338f6dc67b01fc3293d45017091e99c1267b59
emile.zhu <emile.zhu@hotmail.com>
2018-06-04 04:58:45 +0800

    add requirements.txt  and MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
-from distutils.core import setup
+from setuptools import setup
+
+with open('requirements', 'r') as fr:
+  required = fr.read().splitlines()
+
 setup(
   name = 'pyvisgraph',
   packages = ['pyvisgraph'],
-  version = '0.1.9',
+  version = '0.2.1',
   description = 'Given a set of simple obstacle polygons, build a visibility graph and find the shortest path between two points.',
   author = 'Christian Reksten-Monsen',
   author_email = 'christian@reksten-monsen.com',
   url = 'https://github.com/TaipanRex/pyvisgraph',
-  download_url = 'https://github.com/TaipanRex/pyvisgraph/tarball/0.1.9',
+  install_requires = required,
+  download_url = 'https://github.com/TaipanRex/pyvisgraph/tarball/0.2.1',
   keywords = ['visibility', 'graph', 'shortest'],
   classifiers = [],
 )
