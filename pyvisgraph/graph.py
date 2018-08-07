af0c7095a529cdf421d2edcbc99d16a5df519801
jianantian <emile.zhu@hotmail.com>
2018-08-07 19:34:27 +0800

    modify __eq__ method for Point class, the original == for float is not robust. add set_epsilon function to change EPSILON value

--- pyvisgraph/graph.py
+++ pyvisgraph/graph.py
@@ -22,6 +22,16 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 from collections import defaultdict
+EPSILON = 1e-6
+
+
+def set_epsilon(value):
+    """
+    set EPSILON value
+    """
+
+    EPSILON = float(value)
+    return EPSILON
 
 
 class Point(object):
@@ -33,7 +43,7 @@ class Point(object):
         self.polygon_id = polygon_id
 
     def __eq__(self, point):
-        return point and self.x == point.x and self.y == point.y
+        return point and abs(self.x - point.x) + abs(self.y - point.y) < EPSILON
 
     def __ne__(self, point):
         return not self.__eq__(point)
